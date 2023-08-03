6b9cb726609a20da94bd3c2c8acd822472de66d0
dhamolahedonist <dhamolahedonist>
2023-08-03 21:33:47 +0100

    created the server routes and the views

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const path = require("path");
+require("dotenv").config();
+
+const PORT = process.env.PORT;
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.use(express.json());
+app.use(expressLayouts);
+app.set("layout", "./layouts/main");
+
+app.set("view engine", "ejs"); // Replace 'ejs' with your template engine (e.g., 'pug' for Pug)
+app.set("views", path.join(__dirname, "views"));
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use("/", require("./server/routes/main"));
+
+app.listen(PORT, () => {
+  console.log(`magic happens on PORT: http://localhost:${PORT}`);
+});
+
+// module.exports = app;
---
+++ server/routes/main.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+router.get("", (req, res) => {
+  const locals = {
+    title: "NodeJs Blog",
+    description: "Simple Blog created with NodeJs, Express and Mongodb",
+  };
+  res.render("index", locals);
+});
+router.get("/about", (req, res) => {
+  res.render("about");
+});
+
+module.exports = router;
